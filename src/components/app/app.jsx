e905929ac0267e8927b7fec226bf4681b2ac2c34
OlegS <xwx18@mail.ru>
2020-06-17 15:08:19 +0300

    Добавляет и настраивает роутер, экраны с вопросами жанра и исполнителя

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,14 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import WelcomeScreen from '../welcome-screen/welcome-screen.jsx';
+import GuessArtistScreen from '../guess-artist-screen/guess-artist-screen.jsx';
+import GuessGenreScreen from '../guess-genre-screen/guess-genre-screen.jsx';
 
 const App = (props) => {
   const {errorsCount, onWelcomeButtonClick} = props;
 
-  return <WelcomeScreen
-    errorsCount={errorsCount}
-    onWelcomeButtonClick={onWelcomeButtonClick}
-  />;
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <WelcomeScreen
+            errorsCount={errorsCount}
+            onWelcomeButtonClick={onWelcomeButtonClick}
+          />
+        </Route>
+        <Route exact path="/artist">
+          <GuessArtistScreen />
+        </Route>
+        <Route exact path="/genre">
+          <GuessGenreScreen />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
 };
 
 App.propTypes = {
