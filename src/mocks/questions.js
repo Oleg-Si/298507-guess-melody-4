b6df99b9b4543460af7432b97e3d84d1ad3b6c06
Oleg S <xwx18@mail.ru>
2020-06-19 13:08:52 +0300

    Добавляет е2е тест для компонента GuessGenreScreen
    Добавляет е2е тест для компонента GuessGenreScreen

--- src/mocks/questions.js
+++ src/mocks/questions.js
@@ -6,22 +6,22 @@ export default [
     genre: `rock`,
     answers: [
       {
-        id: 1,
+        id: 0,
         src: `https://upload.wikimedia.org/wikipedia/commons/4/4e/BWV_543-fugue.ogg`,
         genre: `rock`,
       },
       {
-        id: 2,
+        id: 1,
         src: `https://upload.wikimedia.org/wikipedia/commons/4/4e/BWV_543-fugue.ogg`,
         genre: `blues`,
       },
       {
-        id: 3,
+        id: 2,
         src: `https://upload.wikimedia.org/wikipedia/commons/4/4e/BWV_543-fugue.ogg`,
         genre: `jazz`,
       },
       {
-        id: 4,
+        id: 3,
         src: `https://upload.wikimedia.org/wikipedia/commons/4/4e/BWV_543-fugue.ogg`,
         genre: `rock`,
       }
@@ -35,17 +35,17 @@ export default [
     },
     answers: [
       {
-        id: 1,
+        id: 0,
         picture: `${AVATAR_URL}${Math.random()}`,
         artist: `Chivas Regal`,
       },
       {
-        id: 2,
+        id: 1,
         picture: `${AVATAR_URL}${Math.random()}`,
         artist: `Jack Daniels`,
       },
       {
-        id: 3,
+        id: 2,
         picture: `${AVATAR_URL}${Math.random()}`,
         artist: `Jim Beam`,
       }
